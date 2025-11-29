076cb65ecd3f8309ff84167697cf1243f4255a3e
diedaricorrea <diedari20diez@gmail.com>
2025-11-28 19:04:42 -0500

    fix: update API and WebSocket URLs in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
-  wsUrl: 'http://localhost:8080/ws',
-  baseUrl: 'http://localhost:8080'
+  apiUrl: 'http://3.238.205.109:8080/api',
+  wsUrl: 'http://3.238.205.109:8080/ws',
+  baseUrl: 'http://3.238.205.109:8080'
 };
