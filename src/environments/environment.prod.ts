5555cb38e89118db91d2147d1072a4368365d338
diedaricorrea <diedari20diez@gmail.com>
2025-11-29 22:48:39 -0500

    fix: update WebSocket URL to use the correct protocol in environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://addventure.up.railway.app/api',
-  wsUrl: 'wss://addventure.up.railway.app/ws',
+  wsUrl: 'https://addventure.up.railway.app/ws', // <--  wss a https capaz se arregla :v
   baseUrl: 'https://addventure.up.railway.app'
 };
-
