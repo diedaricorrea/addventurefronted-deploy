5044d9402b33f593191ad1bc31d89d87e3efeac1
diedaricorrea <diedari20diez@gmail.com>
2025-12-01 12:03:57 -0500

    feat: log current environment during application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { environment } from './environments/environment';
 
+console.log('RUNNING ENVIRONMENT:', environment);
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
